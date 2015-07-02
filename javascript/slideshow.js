1bbb5cf9ca8360e642682a7b02e52d7cfa61e1a0
unknown <rachel.swansen@gmail.com>
2015-07-02 10:43:33 +0200

    added color slideshow

---
+++ javascript/slideshow.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Rachel on 7/1/2015.
+ */
+window.setInterval(function(){
+
+    window.setTimeout(function(){
+        $('div').css('background-image','url(http://sstatic.net/stackexchange/Img/launched-ads/english.png)');
+    },100);
+    $('div').css('background-image','url(http://www.gravatar.com/avatar/e526fad25155097bcda4a97b26d78993?s=32&d=identicon&r=PG)');
+},3000);
